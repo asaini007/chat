3b5f0a131a52da65505322245ed70500aa832014
Akshay Saini <asaini007@gmail.com>
2013-12-11 16:27:22 -0500

    changed Server class to use a Server object instead of doing all the work in the main method. Added helper methods.

--- src/Server.java
+++ src/Server.java
@@ -1,47 +1,76 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 
 public class Server {
-	public static void main(String[] args) throws IOException, InterruptedException {
-		final int portNumber = 12543;
-		ServerSocket receiveServerSocket = new ServerSocket(portNumber);
-		receiveServerSocket.setSoTimeout(100);
-		ArrayList<User> users = new ArrayList<User>();
-		while(true) {
-			try {
-				Socket socket = receiveServerSocket.accept();
-				User newUser = new User(socket);
-				newUser.sendMessage("Enter username: ");
-				users.add(newUser);
-			} catch (SocketTimeoutException e) {}
-			for(int i=0;i<users.size();i++) {
-				User currentUser = users.get(i);
-				if(currentUser.hasDisconnected() && currentUser.hasUserName()) {
-					String goodbye = currentUser.getuserName()+" has left the group.";
-					for(User aUser : users)
-						if(aUser.hasUserName())
-							aUser.sendMessage(goodbye);
-					users.remove(currentUser);
-				} else {
-					String message = currentUser.receiveMessage();
-					if(message!=null) {
-						if(!currentUser.hasUserName()) {
-							currentUser.setuserName(message);
-							String welcome = message + " has joined the group.";
-							for(User aUser : users)
-								if(aUser.hasUserName())
-									aUser.sendMessage(welcome);
-						} else {
-							for(User aUser : users)
-								if(aUser.hasUserName())
-									aUser.sendMessageFrom(currentUser, message);
-						}
-					}
-				}
+	ServerSocket receiveServerSocket;
+	ArrayList<User> users;
+	
+	public Server(int number) {
+		try {
+			receiveServerSocket = new ServerSocket(number);
+		} catch (IOException e) {}
+		try {
+			receiveServerSocket.setSoTimeout(100);
+		} catch (SocketException e) {}
+		users = new ArrayList<User>();
+	}
+	
+	public void receiveNewConnection() {
+		try {
+			Socket socket = receiveServerSocket.accept();
+			User newUser = new User(socket);
+			newUser.sendMessage("Enter username: ");
+			users.add(newUser);
+		} catch (SocketTimeoutException e) {
+		} catch (IOException e) {}
+	}
+	
+	public void cycleThroughUsers() {
+		for(int i=0;i<users.size();i++) {
+			User currentUser = users.get(i);
+			if(!disconnected(currentUser))
+				sendMessage(currentUser);
+		}
+	}
+	
+	public boolean disconnected(User u) {
+		if(u.hasDisconnected() && u.hasUserName()) {
+			String goodbye = u.getuserName()+" has left the group.";
+			for(User aUser : users)
+				if(aUser.hasUserName())
+					aUser.sendMessage(goodbye);
+			users.remove(u);
+			return true;
+		}
+		return false;
+	}
+	
+	public void sendMessage(User u) {
+		String message = u.receiveMessage();
+		if(message!=null) {
+			if(!u.hasUserName()) {
+				u.setuserName(message);
+				String welcome = message + " has joined the group.";
+				for(User aUser : users)
+					if(aUser.hasUserName())
+						aUser.sendMessage(welcome);
+			} else {
+				for(User aUser : users)
+					if(aUser.hasUserName())
+						aUser.sendMessageFrom(u, message);
 			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		Server theServer = new Server(12543);
+		while(true) {
+			theServer.receiveNewConnection();
+			theServer.cycleThroughUsers();
 			Thread.sleep(100);
 		}
 	}
